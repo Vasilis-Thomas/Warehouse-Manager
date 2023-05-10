0b36fd365e87346fa6c20dbeff0add96b863054b
Nsarakenidis <nikoreve@hotmail.gr>
2023-05-10 22:40:15 +0300

    (+) SettingsActivity.java and activity_settings.xml

--- app/src/main/java/com/example/eshopapplication/SettingsActivity.java
+++ app/src/main/java/com/example/eshopapplication/SettingsActivity.java
@@ -1,27 +1,167 @@
 package com.example.eshopapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import signuploginfirebase.LoginActivity;
+import signuploginfirebase.sharedPreferenceConfig;
+
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private signuploginfirebase.sharedPreferenceConfig sharedPreferenceConfig;
     Toolbar toolbar;
-
-    TextView toolbarTitle;
+    private final static String TAG = "com.example.applicationwithmenu (Setting Activity)";
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    TextView username_text, email_text;
+    AlertDialog.Builder builder;
+    TextView toolbarTitle, usernameTxt, emailTxt, reportABugTxt, infoTxt, logoutTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_settings);
+        sharedPreferenceConfig = new sharedPreferenceConfig(getApplicationContext());
 
         makeToolbarButtonArrowBack(toolbar);
+
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        usernameTxt = findViewById(R.id.username_txt);
+        emailTxt = findViewById(R.id.email_txt);
+        reportABugTxt = findViewById(R.id.report_a_bug_txt);
+        infoTxt = findViewById(R.id.info_txt);
+        logoutTxt = findViewById(R.id.logout_txt);
+//        versionTxt = findViewById(R.id.version_txt);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.dr_home:
+                        menuItem.setChecked(false);
+                        startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+//                        Intent intent = new Intent(Logout_Activity.this, LoginActivity.class);
+//                        Bundle bundle = new Bundle();
+//                        bundle.putString("StringKey1", "from :- Logout Activity");
+//                        Supplier_Fragment fv = new Supplier_Fragment();
+//                        fv.setArguments(bundle);
+//                        startActivity(intent);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.dr_contacts:
+                        menuItem.setChecked(true);
+
+                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.dr_find:
+                        menuItem.setChecked(true);
+
+                        return true;
+                    case R.id.dr_marketplace:
+                        menuItem.setChecked(true);
+
+                        drawerLayout.closeDrawers();
+
+                        return true;
+                    case R.id.dr_about:
+                        menuItem.setChecked(true);
+//                        showInfo(builder);
+                        drawerLayout.closeDrawers();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        FirebaseUser firebaseuser = FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseuser != null) {
+            String userName = firebaseuser.getDisplayName();
+            String userEmail = firebaseuser.getEmail();
+            View menu_drawer_head = navigationView.getHeaderView(0);
+            username_text = menu_drawer_head.findViewById(R.id.drawer_menu_header_username_text);
+            email_text = menu_drawer_head.findViewById(R.id.drawer_menu_header_email_text);
+            username_text.setText(userName);
+            email_text.setText(userEmail);
+//        }
+//        else{
+//            View menu_drawer_head = navigationView.getHeaderView(0);
+//            username_text = menu_drawer_head.findViewById(R.id.drawer_menu_header_username_text);
+//            email_text = menu_drawer_head.findViewById(R.id.drawer_menu_header_email_text);
+//            username_text.setText("Username: Unknown");
+//            email_text.setText("Unknown");
+//        }
+            usernameTxt.setText(userName);
+            emailTxt.setText(userEmail);
+
+            reportABugTxt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // IMPLICIT sto gmail
+                }
+            });
+
+            infoTxt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    builder = new AlertDialog.Builder(SettingsActivity.this);
+                    showInfo(builder);
+                }
+            });
+
+
+//        versionTxt.setText(BuildConfig.VERSION_CODE);
+
+            logoutTxt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    builder = new AlertDialog.Builder(SettingsActivity.this);
+                    builder.setTitle("Confirmation message");
+                    builder.setMessage("You are log in as " + userName + ".\nAre you sure you want to logout?");
+                    builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+//                            sharedPreferenceConfig.writeLoginStatus(false);
+//                            startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            sharedPreferenceConfig.writeLoginStatus(false);
+                            startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            });
+        }
     }
 
     @Override
@@ -29,7 +169,8 @@ public class SettingsActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case android.R.id.home:
 //                NavUtils.navigateUpFromSameTask(this);
-                onBackPressed();
+//                onBackPressed();
+                drawerLayout.openDrawer((GravityCompat.START));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -37,15 +178,38 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-    public Toolbar makeToolbarButtonArrowBack(Toolbar tb){
-        tb = findViewById(R.id.toolbar_layout);
-        setSupportActionBar(tb);
+//    public Toolbar makeToolbarButtonArrowBack(Toolbar toolbar){
+//        toolbar = findViewById(R.id.toolbar_layout);
+//        setSupportActionBar(toolbar);
+//        ActionBar actionBar = getSupportActionBar();
+//            Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
+////        actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(R.drawable.ic_round_arrow_back_24);
+//            actionBar.setTitle("");
+//        toolbarTitle = toolbar.findViewById(R.id.toolbar_title);
+//        toolbarTitle.setText(R.string.set_toolbar_title);
+//        return toolbar;
+//    }
+
+    public Toolbar makeToolbarButtonArrowBack(Toolbar toolbar){
+        toolbar = findViewById(R.id.toolbar_layout);
+        Log.d(TAG,"DOYLEPSE PALIO MALAKIA "+ toolbar);
+        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
+//        Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_round_arrow_back_24);
-        actionBar.setTitle("");
-        toolbarTitle = tb.findViewById(R.id.toolbar_title);
-        toolbarTitle.setText(R.string.set_toolbar_title);
-        return tb;
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_navigation_button);
+        return toolbar;
     }
+
+    public static void showInfo(AlertDialog.Builder b){
+        b.setTitle("Application Information").
+                setMessage("Constructors: Nikolas Sarakenidis, Vasilis Thomas\n"
+                        + "Institution: International University of Greece\n"
+                        + "Supervisor: Euklidis Keramopoylos\n"
+                        + "Project name: Economy E-shop Application\n"
+                        + "Application: Android\n"
+                        + "MinimumSdk: 24").show();
+    }
+
 }
